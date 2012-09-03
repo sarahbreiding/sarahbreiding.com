c866678d69c3e47b4319cf8f1f2aab6c77253525
Chris Breiding <chrisbreiding@gmail.com>
2012-09-03 17:26:23 -0400

    Add contact form

--- app.rb
+++ app.rb
@@ -1,4 +1,7 @@
 require 'sinatra'
+require 'pony'
+require 'maruku'
+require_relative '../secrets.rb'
 
 get '/' do
   @title = 'Sarah Breiding | Digital Library &amp; Information Professional'
@@ -22,4 +25,25 @@ get '/contact/?' do
   @title = 'Contact | Sarah Breiding'
   @slug = 'contact'
   erb :contact
+end
+
+post '/send-message' do
+  Pony.mail({
+    to: 'breiding.sarah@gmail.com',
+    subject: "Website Contact Form Submission from #{params[:name]}",
+    html_body: erb(:email, layout: false),
+
+    via: :smtp,
+    via_options: {
+      address: 'smtp.gmail.com',
+      port: '587',
+      enable_starttls_auto: true,
+      user_name: 'breiding.sarah@gmail.com',
+      password: GAPPS_PW,
+      authentication: :plain,
+      domain: 'sarahbreiding.com'
+    }
+  })
+
+  '<div class="response valid">Your message has been sent - Thank you!</div>'
 end
