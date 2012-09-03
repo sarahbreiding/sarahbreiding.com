ab010761430790ed98b6a4117079de9e9f10e9b7
Chris Breiding <chrisbreiding@gmail.com>
2012-09-03 14:43:04 -0400

    Add active nav style

--- app.rb
+++ app.rb
@@ -2,20 +2,24 @@ require 'sinatra'
 
 get '/' do
   @title = 'Sarah Breiding | Digital Library &amp; Information Professional'
+  @slug = 'home'
   erb :index
 end
 
 get '/about/?' do
   @title = 'About | Sarah Breiding'
+  @slug = 'about'
   erb :about
 end
 
 get '/portfolio/?' do
   @title = 'Portfolio | Sarah Breiding'
+  @slug = 'portfolio'
   erb :portfolio
 end
 
 get '/contact/?' do
   @title = 'Contact | Sarah Breiding'
+  @slug = 'contact'
   erb :contact
 end
