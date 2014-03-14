8f1b11340986c3d0594ff0ae6e15b1da99ab8534
chrisbreiding <chrisbreiding@gmail.com>
2014-03-14 17:17:31 -0400

    use mandrill for contact message email and clean up formatting

--- script.js
+++ script.js
@@ -1,39 +1,75 @@
-$.fn.sarahsSlideToggle = function () {
-	return this.each(function () {
-		var $el = $(this);
-		if ($el.is(':visible')) {
-			$el.animate({ width: 0 }, 1000, function () {
-				$el.hide();
-			});
-		} else {
-			$el.css({ width: 0 });
-			$el.show();
-			$el.animate({ width: '100%' }, 1000);
-		}	
-	});
-};
-	
-$(document).ready(function() {
-	var $contactForm = $('#contact-form');
-	$.get('http://courier.crbapps.com/wake');
+(function () {
+
+  $.fn.sarahsSlideToggle = function () {
+    return this.each(function () {
+      var $el = $(this);
+      if ($el.is(':visible')) {
+        $el.animate({ width: 0 }, 750, function () {
+          $el.hide();
+        });
+      } else {
+        $el.css({ width: 0 });
+        $el.show();
+        $el.animate({ width: '100%' }, 750);
+      }
+    });
+  };
+
+  var reverse = function(word) {
+    return word.split('').reverse().join('');
+  };
+
+  var $contactForm = $('#contact-form');
+
+  var messageData = function () {
+    var name = $('#name').val();
+    var email = $('#email').val();
+    var message = $('#message').val();
+
+    return {
+      key: 'rPNtGh3XdGX45uKEpeZGjA',
+      template_name: 'website-contact-form',
+      template_content: [
+        { name: 'contact-name', content: name },
+        { name: 'contact-email', content: email },
+        { name: 'contact-message', content: message }
+      ],
+      message: {
+        from_name: name,
+        from_email: email,
+        to: [{ email: [reverse('haras.gnidierb'), reverse('moc.liamg')].join('@') }]
+      }
+    };
+  };
+
+  var sendMessage = function () {
+    return $.ajax({
+      type: 'POST',
+      dataType: 'JSON',
+      url: 'https://mandrillapp.com/api/1.0/messages/send-template.json',
+      data: messageData()
+    });
+  };
+
+  $(function() {
 
     $(".envelope").click(function(e) {
-        e.preventDefault();
-        $(".contact").sarahsSlideToggle();
+      e.preventDefault();
+      $(".contact").sarahsSlideToggle();
     });
 
-	$contactForm.submit(function (e) {
-        e.preventDefault();
-      	$('#submit-contact').after($('<div class="processing">Sending&hellip;</div>')).remove();
-		$.ajax({
-	      dataType: 'JSONP',
-	      url: 'http://courier.crbapps.com/send',
-	      data: $contactForm.serialize(),
-	      success: function (response) {
-		    $contactForm.slideUp(500, function() {
-		      $contactForm.after("<div class='response'>" + response + "</div").remove();
-		    });
-	      }
-	    });
-	});	    	
-});
+    $contactForm.submit(function (e) {
+      e.preventDefault();
+
+      $('#submit').after($('<div class="processing">Sending&hellip;</div>')).remove();
+
+      sendMessage().then(function () {
+        $contactForm.slideUp(500, function() {
+          $contactForm.after("<div class='response'>Your message has been sent - Thank you!</div").remove();
+        });
+      });
+    });
+
+  });
+
+}());
