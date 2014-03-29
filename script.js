bab61ea8762d1e21d62d08ad14635719de59d72b
chrisbreiding <chrisbreiding@gmail.com>
2014-03-29 15:09:39 -0400

    use courier for sending contact form message

--- script.js
+++ script.js
@@ -15,61 +15,32 @@
     });
   };
 
-  var reverse = function(word) {
-    return word.split('').reverse().join('');
-  };
-
   var $contactForm = $('#contact-form');
-
-  var messageData = function () {
-    var name = $('#name').val();
-    var email = $('#email').val();
-    var message = $('#message').val();
-
-    return {
-      key: 'rPNtGh3XdGX45uKEpeZGjA',
-      template_name: 'website-contact-form',
-      template_content: [
-        { name: 'contact-name', content: name },
-        { name: 'contact-email', content: email },
-        { name: 'contact-message', content: message }
-      ],
-      message: {
-        from_name: name,
-        from_email: email,
-        to: [{ email: [reverse('haras.gnidierb'), reverse('moc.liamg')].join('@') }]
-      }
-    };
-  };
 
   var sendMessage = function () {
     return $.ajax({
       type: 'POST',
       dataType: 'JSON',
-      url: 'https://mandrillapp.com/api/1.0/messages/send-template.json',
-      data: messageData()
+      url: 'http://courier.crbapps.com',
+      data: $contactForm.serialize()
     });
   };
 
-  $(function() {
+  $(".envelope").click(function(e) {
+    e.preventDefault();
+    $(".contact").sarahsSlideToggle();
+  });
 
-    $(".envelope").click(function(e) {
-      e.preventDefault();
-      $(".contact").sarahsSlideToggle();
-    });
+  $contactForm.submit(function (e) {
+    e.preventDefault();
 
-    $contactForm.submit(function (e) {
-      e.preventDefault();
+    $('#submit').after($('<div class="processing">Sending&hellip;</div>')).remove();
 
-      $('#submit').after($('<div class="processing">Sending&hellip;</div>')).remove();
-
-      sendMessage().then(function () {
-        $contactForm.slideUp(500, function() {
-          $contactForm.after("<div class='response'>Your message has been sent - Thank you!</div").remove();
-        });
+    sendMessage().then(function () {
+      $contactForm.slideUp(500, function() {
+        $contactForm.after("<div class='response'>Your message has been sent - Thank you!</div").remove();
       });
     });
-
   });
 
 }());
