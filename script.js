a8474ff97b23a34ff7b4ea02db5d6c3e2aea5998
Sarah Breiding <breiding.sarah@gmail.com>
2018-10-18 13:40:38 -0400

    site rebuild

--- script.js
+++ script.js
@@ -1,42 +1,28 @@
 (function () {
-  $('.contact-handle').click(function(){
-    $('.contact-form-section').toggleClass('slideUp slideDown');
-    $('.hidden-contact-section').slideToggle();
+  $('.mail-button').click(function(e){
+    e.preventDefault();
+
+    $('.contact-form-section').toggleClass('hidden');
   })
 
-  $('.flex-item-1').click(function(){
-    $(this).toggleClass('showAboutText');
-    $('.flex-item-2').toggleClass('hideSocialMedia');
-    $('.about-text').slideToggle('slow');
+  $('.close-form').click(function(e){
+    e.preventDefault();
+
+    $('.contact-form-section').toggleClass('hidden');
   });
-
-  $('.flex-item-4').click(function(){
-    $(this).toggleClass('showWorkExamples');
-    $('.flex-item-4 h1').toggleClass('moveWorkHeader');
-    $('.flex-item-3').toggleClass('hideRubyTop');
-    $('.work-examples').slideToggle('slow');
-  })
-
-  $('.flex-item-4 img').click(function(){
-    var imgName = $(this).attr('class');
-    $('#' + imgName).show();
-    $('.light-box').click(function(){
-      $('.light-box').hide();
-    })
-  })
 
   var $contactForm = $('#contact-form');
 
-  $contactForm.submit(function (e) {
+  $contactForm.submit(function(e){
     e.preventDefault();
 
     $('#submit').after($('<div class="processing">Sending&hellip;</div>')).remove();
 
-    $.post($contactForm.attr('action'), $contactForm.serialize()).then(function () {
+   // $.post($contactForm.attr('action'), $contactForm.serialize()).then(function () {
       $contactForm.slideUp(500, function() {
         $contactForm.after("<div class='response'>Your message has been sent - Thank you!</div").remove();
       });
-    });
+    //});
   });
 
 }());
